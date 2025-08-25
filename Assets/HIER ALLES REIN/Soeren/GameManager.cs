79aad3c4eaf8dfadf35e0530c54126a17f1a37fd
Sören Irlenbusch <176750049+BlackPhoenix-21@users.noreply.github.com>
2025-08-25 10:33:33 +0200

    Chris ist ein penner

--- Assets/HIER ALLES REIN/Soeren/GameManager.cs
+++ Assets/HIER ALLES REIN/Soeren/GameManager.cs
@@ -1,7 +1,6 @@
-using NUnit.Framework;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -10,6 +9,7 @@ public class GameManager : MonoBehaviour
     public CharacterData playerData;
 
     public List<GameObject> Level = new();
+    public GameObject currentLevel;
 
     [HideInInspector]
     public bool isTransitioning = false;
@@ -31,10 +31,85 @@ public class GameManager : MonoBehaviour
     {
         Level.AddRange(GameObject.FindGameObjectsWithTag("Level"));
         Level.Sort((a, b) => a.name.CompareTo(b.name));
+        currentLevel = Level[0];
         for (int i = 1; i < Level.Count; i++)
         {
             Level[i].SetActive(false);
         }
+        gameObject.AddComponent<Fade>();
+    }
+}
+
+public class Fade : MonoBehaviour
+{
+    public string targetSceneName;
+    public GameObject spawnPosition;
+    public CanvasGroup fadeCanvasGroup;
+    public float fadeDuration = 1f;
+
+    public bool isTransitioning = false;
+    public IEnumerator FadeAndSwitchScene()
+    {
+        isTransitioning = true;
+        GameManager.instance.isTransitioning = true;
+
+        // Blende einblenden (Alpha von 0 auf 1)
+        float timer = 0f;
+        while (timer < fadeDuration)
+        {
+            timer += Time.deltaTime;
+            fadeCanvasGroup.alpha = Mathf.Lerp(0f, 1f, timer / fadeDuration);
+            yield return null;
+        }
+        fadeCanvasGroup.alpha = 1f;
+
+        // Szene wechseln
+        foreach (GameObject level in GameManager.instance.Level)
+        {
+            if (level == GameManager.instance.currentLevel)
+            {
+                continue;
+            }
+            level.SetActive(false);
+        }
+
+        GameObject targetLevel = GameManager.instance.Level.Find(x => x.name == targetSceneName);
+        if (targetLevel != null)
+        {
+            targetLevel.SetActive(true);
+
+            // Optional: Spawnposition kann in der neuen Szene genutzt werden
+            GameObject player = GameObject.FindWithTag("Player");
+            if (player != null)
+            {
+                player.transform.position = spawnPosition.transform.position;
+            }
+
+            // Starte FadeOut nur, wenn dieses GameObject aktiv ist
+            if (targetLevel.activeInHierarchy)
+            {
+                StartCoroutine(FadeOut());
+            }
+        }
+    }
+
+    private IEnumerator FadeOut()
+    {
+        isTransitioning = false;
+        GameManager.instance.isTransitioning = false;
+        GameManager.instance.currentLevel.gameObject.SetActive(false);
+
+        // Blende einblenden (Alpha von 1 auf 0)
+        float timer = 0f;
+        while (timer < fadeDuration)
+        {
+            timer += Time.deltaTime;
+            fadeCanvasGroup.alpha = Mathf.Lerp(1f, 0f, timer / fadeDuration);
+            yield return null;
+        }
+        fadeCanvasGroup.alpha = 0f;
+
+        GameManager.instance.currentLevel = GameManager.instance.Level.Find(x => x.name == targetSceneName);
     }
 }
 
--- Assets/HIER ALLES REIN/Soeren/SceneTransitionTrigger.cs
+++ Assets/HIER ALLES REIN/Soeren/SceneTransitionTrigger.cs
@@ -1,6 +1,4 @@
-using System.Collections;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class SceneTransitionTrigger : MonoBehaviour
 {
@@ -10,56 +8,16 @@ public class SceneTransitionTrigger : MonoBehaviour
     public CanvasGroup fadeCanvasGroup;
     public float fadeDuration = 1f;
 
-    private bool isTransitioning = false;
-
     private void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log("Trigger entered: " + other.name);
-        if (!isTransitioning && other.CompareTag("Player"))
+        Fade fade = GameManager.instance.gameObject.GetComponent<Fade>();
+        if (!fade.isTransitioning && other.CompareTag("Player"))
         {
-            StartCoroutine(FadeAndSwitchScene());
+            fade.targetSceneName = targetSceneName;
+            fade.spawnPosition = spawnPosition;
+            fade.fadeCanvasGroup = fadeCanvasGroup;
+            fade.fadeDuration = fadeDuration;
+            StartCoroutine(fade.FadeAndSwitchScene());
         }
-    }
-
-    private IEnumerator FadeAndSwitchScene()
-    {
-        isTransitioning = true;
-        GameManager.instance.isTransitioning = true;
-
-        // Blende einblenden (Alpha von 0 auf 1)
-        float timer = 0f;
-        while (timer < fadeDuration)
-        {
-            timer += Time.deltaTime;
-            fadeCanvasGroup.alpha = Mathf.Lerp(0f, 1f, timer / fadeDuration);
-            yield return null;
-        }
-        fadeCanvasGroup.alpha = 1f;
-
-        // Szene wechseln
-        foreach (GameObject level in GameManager.instance.Level)
-        {
-            level.SetActive(false);
-        }
-        GameManager.instance.Level.Find(x => x.name == targetSceneName).SetActive(true);
-        // Optional: Spawnposition kann in der neuen Szene genutzt werden
-        GameObject.FindWithTag("Player").transform.position = spawnPosition.transform.position;
-        StartCoroutine(FadeOut());
-    }
-
-    private IEnumerator FadeOut()
-    {
-        isTransitioning = false;
-        GameManager.instance.isTransitioning = false;
-
-        // Blende einblenden (Alpha von 1 auf 0)
-        float timer = 0f;
-        while (timer < fadeDuration)
-        {
-            timer += Time.deltaTime;
-            fadeCanvasGroup.alpha = Mathf.Lerp(1f, 0f, timer / fadeDuration);
-            yield return null;
-        }
-        fadeCanvasGroup.alpha = 0f;
     }
 }
